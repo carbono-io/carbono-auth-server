7af86fe65882a3b2210b829d1ff12277b7e423a1
Daniel Moura <daniel.moura@cesar.org.br>
2015-09-04 15:34:36 -0300

    Initial version

---
+++ index.js
@@ -0,0 +1,32 @@
+/**
+ * Cabono Authentication Server (based on oauth2-server npm page)
+ * 
+ * @author Daniel Moura
+ */
+
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    oauthserver = require('oauth2-server'),
+    memorystore = require('model.js');
+ 
+var app = express();
+ 
+app.use(bodyParser.urlencoded({ extended: true }));
+ 
+app.use(bodyParser.json());
+ 
+app.oauth = oauthserver({
+  model: memorystore,
+  grants: ['password','refresh_token'],
+  debug: true
+});
+ 
+app.all('/oauth/token', app.oauth.grant());
+ 
+app.get('/', app.oauth.authorise(), function (req, res) {
+  res.send('Secret area');
+});
+ 
+app.use(app.oauth.errorHandler());
+ 
+app.listen(3000);
