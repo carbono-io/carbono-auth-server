d6aab733a942efa0d5a9f2112c1586dae9811d88
Aline Takechi <akt@cesar.org.br>
2015-09-21 19:08:03 -0300

    [JRD-517] oauth server accepting client credentials in request body

--- app/lib/auth.js
+++ app/lib/auth.js
@@ -3,11 +3,12 @@
 // Load required packages
 var passport = require('passport');
 var BasicStrategy = require('passport-http').BasicStrategy;
+var ClientStrategy = require('passport-oauth2-client-password').Strategy;
 var Client = require('./models/client');
 var imperial = require('./bromelia-imperial-cli');
 
 passport.use(new BasicStrategy(
-    function (username, password, callback){
+    function (username, password, callback) {
         imperial.authenticate(username, password, callback);
     }
 ));
@@ -28,8 +29,22 @@ passport.use('client-basic', new BasicStrategy(
     }
 ));
 
+passport.use(new ClientStrategy(
+    function (clientId, clientSecret, done) {
+        Client.findOne({ id: clientId }, function (err, client) {
+            if (err) { return done(err); }
+            if (!client) { return done(null, false); }
+            if (client.secret !== clientSecret) {
+                return done(null, false);
+            }
+            return done(null, client);
+        });
+    }
+));
+
 exports.isAuthenticated = passport.authenticate(['basic'],
     { session: false });
 
-exports.isClientAuthenticated = passport.authenticate('client-basic',
+exports.isClientAuthenticated = passport.authenticate(
+    ['client-basic', 'oauth2-client-password'],
     { session: false });
